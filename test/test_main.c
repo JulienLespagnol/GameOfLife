99117a70337a4f03903cd07cdd02c0d61ba3fe1e
Julien Lespagnol <lespagnolj@gmail.com>
2020-11-08 16:52:05 +0100

    add .cproject to get builds infos add test target and main

---
+++ test/test_main.c
@@ -0,0 +1,13 @@
+/*
+ * test_main.c
+ *
+ *  Created on: 8 nov. 2020
+ *      Author: julien
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Test Target"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
